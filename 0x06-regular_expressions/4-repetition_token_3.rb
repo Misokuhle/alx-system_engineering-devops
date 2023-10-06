446300ea23a18e536240474965c1f287bf3a8be6
Misokuhle <misokuhle46@gmail.com>
2023-10-06 15:28:50 -0700

    Updated

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,6 +1,2 @@
 #!/usr/bin/env ruby
-# Repetition Token #3
-
-# puts ARGV[0].scan(/^hbt+n/).join
-# OR
 puts ARGV[0].scan(/hbt*n/).join
--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,5 +1,2 @@
 #!/usr/bin/env ruby
-
-# puts ARGV[0].scan(/^h.n$/).join
-# OR
-puts ARGV[0].scan(/h.n/).join
+puts ARGV[0].scan(/^h.n$/).join
